aa1acb66bc34c38648b50dd36acd41288d7ebeb8
shrutiy14 <syamala@ucsd.edu>
2025-02-23 19:17:45 -0800

    Update map.js

--- map.js
+++ map.js
@@ -2,3 +2,16 @@
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 // Check that Mapbox GL JS is loaded
 console.log("Mapbox GL JS Loaded:", mapboxgl);
+
+  // Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2hydXRpeTE0IiwiYSI6ImNtN2lmc3NwZzAxcHcybnBrOWQyYm1ndDcifQ.vHrxszYZLcMHJ6-G2KnSmg';
+
+  // Initialize the map
+const map = new mapboxgl.Map({
+    container: 'map', // ID of the div where the map will render
+    style: 'mapbox://styles/mapbox/streets-v12', // Map style
+    center: [-71.09415, 42.36027], // [longitude, latitude]
+    zoom: 12, // Initial zoom level
+    minZoom: 5, // Minimum allowed zoom
+    maxZoom: 18 // Maximum allowed zoom
+});
